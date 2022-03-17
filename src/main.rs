fa806557f191dc3613371f47a232724026f0c6d4
Yassin <81421290+Yacheen@users.noreply.github.com>
2022-03-17 14:27:10 -0600

    I somehow pulled through debuggin post logic with the help of my friend Zeychin

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
+use actix_web::{HttpResponseBuilder};
 //serde and env var stuff
 use dotenv::dotenv;
 use std::env;
-use std::io;
-use serde::Deserialize;
+use serde::Deserialize; 
 use reqwest;
 
 
 //actix web
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, cookie::Cookie, HttpRequest, HttpMessage, http::StatusCode};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, cookie::Cookie, http::StatusCode};
 
 
-#[derive(Deserialize)] struct Summoner { puuid: String }
+#[derive(Deserialize, Debug)]
+struct SynergiesPostBody {
+  username: String
+}
+#[derive(Deserialize, Debug)] struct Summoner { puuid: String }
 #[derive(Deserialize)] struct MatchIds (String);
 //begin pepega json deserialization:
 #[derive(Deserialize)] struct Game { info: GameInfo }
@@ -23,17 +27,33 @@ async fn hello() -> impl Responder {
   HttpResponse::Ok().body("Hewwo wowld!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-  let code = StatusCode::OK;
-  let cookie = Cookie::new("authi", &req_body);
-  println!("{}", cookie);
-  match HttpResponse::add_cookie(&mut HttpResponse::new(code), &cookie) {
-    Ok(val) => val,
-    Err(_) => println!("err")
-  };
+#[post("/api/synergies")]
+async fn echo(synergiespostdata: web::Json<SynergiesPostBody>) -> impl Responder {
+  dotenv().ok();
+  let api_key = env::var("API_KEY").unwrap();
+  let url = format!("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key={}", &synergiespostdata.0.username, api_key);
+  //get puuid
+  let summoner =  reqwest::get(url).await.unwrap().json::<Summoner>().await.unwrap();
+
+  let matches_url = format!("https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{}/ids?api_key={}", summoner.puuid, api_key);
+
+  //get match ids by puuid
+  let match_ids = reqwest::get(matches_url).await.unwrap().json::<Vec<MatchIds>>().await.unwrap();
+
+  //change this to a special cookie with cookiebuilder
+  let cookie = Cookie::new("username", &synergiespostdata.0.username);
+
+  for (index, match_id) in match_ids.iter().enumerate() {
+    if index == 2 {break}
+    let match_url = format!("https://americas.api.riotgames.com/lol/match/v5/matches/{}?api_key={}", match_id.0, api_key);
+    let game = reqwest::get(match_url).await.unwrap().json::<Game>().await.unwrap();
+    println!("{:#?}", game.info.participants.get(0..2).unwrap());
+  }
   
-  HttpResponse::Ok().body(req_body)
+  let res = HttpResponseBuilder::new(StatusCode::OK)
+      .cookie(cookie)
+      .body(synergiespostdata.0.username);
+  return res
 }
 
 async fn manual_hello() -> impl Responder {
@@ -43,9 +63,8 @@ async fn manual_hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     
-    //set env vars
-    //dotenv().ok();
-    //let api_key = env::var("API_KEY").unwrap();
+
+
     HttpServer::new(|| {
       App::new()
           .service(hello)
@@ -55,29 +74,5 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
-   
-    // // take input for querying api
-    // let mut username = String::new();
-    // println!("Please enter a username to find...");
 
-    // io::stdin().read_line(&mut username)?;
-
-    
-    // let url = format!("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key={}", username, api_key);
-    // //get puuid
-    // let summoner =  reqwest::get(url).await?.json::<Summoner>().await?;
-
-    // let matches_url = format!("https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{}/ids?api_key={}", summoner.puuid, api_key);
-    // //get match ids by puuid
-    // let match_ids = reqwest::get(matches_url).await?.json::<Vec<MatchIds>>().await?;
-
-    // //for each match id, get match participants name, champ, and if they won (max of 3 matches)
-    // for (index, match_id) in match_ids.iter().enumerate() {
-    //   if index == 2 {break}
-    //   let match_url = format!("https://americas.api.riotgames.com/lol/match/v5/matches/{}?api_key={}", match_id.0, api_key);
-    //   let game = reqwest::get(match_url).await?.json::<Game>().await?;
-    //   println!("{:#?}", game.info.participants);
-    // }
-
-   // Ok(())
 }
