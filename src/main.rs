525653bbf94822537a006f42f9b9fde15af6ddef
Yassin <81421290+Yacheen@users.noreply.github.com>
2022-03-18 10:02:45 -0600

    added summoneryouplayedwith logic

--- src/main.rs
+++ src/main.rs
@@ -10,17 +10,39 @@ use reqwest;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, cookie::Cookie, http::StatusCode};
 
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct SynergiesPostBody {
   username: String
 }
-#[derive(Deserialize, Debug)] struct Summoner { puuid: String }
+#[derive(Deserialize)] struct Summoner { puuid: String }
 #[derive(Deserialize)] struct MatchIds (String);
 //begin pepega json deserialization:
+//structs for sequence of requesting someones info
 #[derive(Deserialize)] struct Game { info: GameInfo }
 #[derive(Deserialize)] struct GameInfo {participants: Vec<Participant>}
-#[derive(Deserialize, Debug)] struct Participant {championName: String, summonerName: String, win: bool}
-//structs for sequence of requesting someones info
+#[derive(Deserialize)] struct Participant {championName: String, summonerName: String, win: bool}
+
+//then put data into array of SummonersYouPLayedWith
+struct SummonerYouPlayedWithInfo { summonerName: String, champions: ChampionsInfo }
+impl SummonerYouPlayedWithInfo {
+  fn new(summonerName: String, champions: ChampionsInfo) -> SummonerYouPlayedWithInfo{
+    SummonerYouPlayedWithInfo {
+      summonerName,
+      champions
+    }
+  }
+}
+
+struct ChampionsInfo { championName: String, wins: u8, losses: u8 }
+impl ChampionsInfo {
+  fn new(championName: String, wins: u8, losses: u8) -> ChampionsInfo {
+    ChampionsInfo {
+      championName,
+      wins,
+      losses
+    }
+  }
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -29,26 +51,48 @@ async fn hello() -> impl Responder {
 
 #[post("/api/synergies")]
 async fn echo(synergiespostdata: web::Json<SynergiesPostBody>) -> impl Responder {
+  //i guess do redis stuff here regarding timeouts when u can refresh data
+  
+
   dotenv().ok();
   let api_key = env::var("API_KEY").unwrap();
-  let url = format!("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key={}", &synergiespostdata.0.username, api_key);
+
+  
   //get puuid
+  let url = format!("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{}?api_key={}", &synergiespostdata.0.username, api_key);
   let summoner =  reqwest::get(url).await.unwrap().json::<Summoner>().await.unwrap();
 
+//get match ids by puuid
   let matches_url = format!("https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{}/ids?api_key={}", summoner.puuid, api_key);
-
-  //get match ids by puuid
   let match_ids = reqwest::get(matches_url).await.unwrap().json::<Vec<MatchIds>>().await.unwrap();
 
   //change this to a special cookie with cookiebuilder
   let cookie = Cookie::new("username", &synergiespostdata.0.username);
 
+  //foreach match_id, request
+  let mut match_data: Vec<SummonerYouPlayedWithInfo> = Vec::new();
   for (index, match_id) in match_ids.iter().enumerate() {
     if index == 2 {break}
     let match_url = format!("https://americas.api.riotgames.com/lol/match/v5/matches/{}?api_key={}", match_id.0, api_key);
     let game = reqwest::get(match_url).await.unwrap().json::<Game>().await.unwrap();
-    println!("{:#?}", game.info.participants.get(0..2).unwrap());
+    //add each game to a list and edit an array to havecertain values based on participant
+    
+    //initizalize SummonerYouPlayedWith
+    let champions_info = ChampionsInfo::new(
+    game.info.participants.get(index).unwrap().championName.to_string(),
+    if game.info.participants.get(index).unwrap().win == true {1} else {0},
+    if game.info.participants.get(index).unwrap().win == true {0} else {1}
+     );
+
+    let summoner_you_played_with = SummonerYouPlayedWithInfo::new(
+      game.info.participants.get(index).unwrap().summonerName.clone(),
+       champions_info
+    );
+
+    match_data.push(summoner_you_played_with);
+    //println!("{:#?}", game.info.participants.get(0..).unwrap());
   }
+
   
   let res = HttpResponseBuilder::new(StatusCode::OK)
       .cookie(cookie)
