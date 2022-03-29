64e461a996b3e761a643b93801dd355a6bb26fef
Yassin <81421290+Yacheen@users.noreply.github.com>
2022-03-29 01:41:33 -0600

    completed api/na/synergies logic

--- src/main.rs
+++ src/main.rs
@@ -26,12 +26,13 @@ struct SynergiesPostBody {
 #[derive(Deserialize)] struct Participant {summonerName: String, championName: String, win: bool, teamId: u8}
 
 //CHANGE THIS 
-#[derive(Deserialize, Serialize)] struct Matches {amount_of_games: u8, games: SummonerYouPlayedWithInfo}
+#[derive(Deserialize, Serialize)] struct Matches {amount_of_games: u8, games: Winrates}
 impl Matches {
   fn new() -> Matches {
+    let games = Winrates { your_team: Vec::new(), enemy_team: Vec::new() };
     Matches {
       amount_of_games: 0,
-      games: Vec::new()
+      games
     }
   }
 }
@@ -39,10 +40,10 @@ impl Matches {
 
 
 #[derive(Serialize, Debug, Deserialize)]
-struct SummonerYouPlayedWithInfo { your_team: Vec<ChampionsInfo>, enemy_team: Vec<ChampionsInfo> }
-impl SummonerYouPlayedWithInfo {
-  fn new(your_team: Vec<ChampionsInfo>, enemy_team: Vec<ChampionsInfo>) -> SummonerYouPlayedWithInfo {
-    SummonerYouPlayedWithInfo {
+struct Winrates { your_team: Vec<ChampionsInfo>, enemy_team: Vec<ChampionsInfo> }
+impl Winrates {
+  fn new(your_team: Vec<ChampionsInfo>, enemy_team: Vec<ChampionsInfo>) -> Winrates{
+    Winrates {
       your_team,
       enemy_team
     }
@@ -100,29 +101,26 @@ async fn synergies(synergiespostdata: web::Json<SynergiesPostBody>) -> impl Resp
         })
         .buffer_unordered(CONCURRENT_REQUESTS);
   
-        
+        //while there are still games left to iterate through,
+        //for each game, get user_teamId
+        //go through each person, if their id is the same, add the champ
+        //they played to your team, otherwise add to enemy team
         while let Some(game) = games.next().await {
           match game {
             Ok(game) =>  {
               match_data.amount_of_games += 1;
-              //get user_teamId somehow here
-              let user_teamId: u8 = game.info.participants.iter().find(|person| {
-                if person.summonerName == synergiespostdata.0.username {
-                  return person.teamId
-                }
-                else {
-                  return ()
-                }
-                
-                
-              });
               
+              // get users team_id
+              let mut user_team_id: u8 = 0;
               for person in game.info.participants.iter() {
-              //change all logic to:
-               //you have champs you played with on your team and the enemy team
-               //add win/loss of the champ to your_team if personId == selfId, otherwise add win/loss to enemy_team
+                if person.summonerName == synergiespostdata.0.username {
+                  user_team_id = person.teamId
+                }
+              }
 
-                if let 100 = person.teamId {
+              for person in game.info.participants.iter() {
+                //if person is on your team, add to your_team, otherwise add to enemy_team
+                if user_team_id == person.teamId {
                   //find a champ, if it destructures into a champ, add a win or loss, otherwise push a new champ
                   if let Some(champ) = match_data.games.your_team.iter_mut().find(|champ| champ.championName == person.championName) {
                     if let true = person.win { champ.wins += 1; } else { champ.losses += 1; }
@@ -137,45 +135,23 @@ async fn synergies(synergiespostdata: web::Json<SynergiesPostBody>) -> impl Resp
                   }
                 }
                 else {
-                  //go through enemy team
-
-                }
-
-
-
-
-
-                //previous syntax
-                if let Some(summ) = match_data.games.iter_mut().find(|summ| summ.summonerName == person.summonerName) {
-                if let Some(champ) = summ.champions.iter_mut().find(|champ| champ.championName == person.championName) {
-                  if let true = person.win { champ.wins += 1; } else { champ.losses += 1; }
-                  
-                  } else {
-                    //determine which team to push to by teamId
-                    summ.champions.push(ChampionsInfo::new(
+                  //go through enemy team for whether to psuh a new champ or edit one
+                  if let Some(champ) = match_data.games.enemy_team.iter_mut().find(|champ| champ.championName == person.championName) {
+                    if let true = person.win { champ.wins += 1; } else { champ.losses += 1; }
+                  }
+                  else {
+                    match_data.games.enemy_team.push(ChampionsInfo::new(
                       person.championName.to_string(),
                       if person.win == true {1} else {0},
-                      if person.win == true {0} else {1}
+                      if person.win == true {0} else {1},
+                      person.teamId
                     ));
-                    
                   }
-              } else {
-                //a champ needs to be added as a vector initially. I probably did this in the worst way possible.
-                let mut champ = Vec::new();
-                champ.push(ChampionsInfo::new(
-                  person.championName.to_string(),
-                  if person.win == true {1} else {0},
-                  if person.win == true {0} else {1}
-                ));
-                match_data.games.push(SummonerYouPlayedWithInfo::new(person.summonerName.clone(),champ));
-                
-              }
-  
+                }
             }
           },
             Err(_) => break
           }
-          
         }
         
         web::Json(match_data)
@@ -189,12 +165,7 @@ async fn synergies(synergiespostdata: web::Json<SynergiesPostBody>) -> impl Resp
     let no_games_found = Matches::new();
     web::Json(no_games_found) 
   }
-
-  
-  
 }
-
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
