b08f55812ea00e9d7309a9365a26a4899586c0c6
Yassin <81421290+Yacheen@users.noreply.github.com>
2022-06-30 01:04:08 -0600

    todo: prevent users from making requests to riot api if rate limited

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 use dotenv::dotenv;
-use std::{ error::Error, env, time::SystemTime};
+use std::{ error::Error, env, time::SystemTime, sync::Mutex};
 use actix_cors::Cors;
 //db
 use mongodb::{bson::doc, options::{ResolverConfig, ClientOptions}};
 //actix web
-use actix_web::{post, web, App, HttpServer, Responder};
+use actix_web::{post, web::{self, Data}, App, HttpServer, Responder, dev::Server};
 //use other files
 mod functions;
 mod definitions;
 const DB_NAME: &str = "myFirstDatabase";
 const COLL_NAME: &str = "summoners";
 
-//request body for /api/synergies
+struct ServerCurrentStates {
+    ran_out_of_requests: bool,
+}
 
+//request body for /api/synergies
 #[post("/api/synergies")]
-async fn synergies(client: web::Data<mongodb::Client>, synergiespostdata: web::Json<definitions::SynergiesPostBody>) -> Result<impl Responder, Box<dyn Error>> {
+async fn synergies(client: Data<mongodb::Client>, server_current_states: Data<Mutex<ServerCurrentStates>>, synergiespostdata: web::Json<definitions::SynergiesPostBody>) -> Result<impl Responder, Box<dyn Error>> {
     //env vars/data initialization
     dotenv().ok();
     //check db
@@ -35,7 +38,7 @@ async fn synergies(client: web::Data<mongodb::Client>, synergiespostdata: web::J
         None => {
             //hit rito db for 75 games if its their first time
             if let Some(match_data) = functions::fetch_matches_from_riot_api(&synergiespostdata.0, 75).await {
-             
+                // assure its at least 50 games, otherwise, dont input into db and spit error msg 
                 //if u can get matches, add them to db,
                 let insert_result = summoners_collection.insert_one(&match_data, None).await?;
                 println!("added person and their games to db: {:#?}", insert_result);
@@ -75,15 +78,18 @@ async fn main() -> std::io::Result<()> {
     //initialize env vars and db
     let mongodb_uri = env::var("MONGODB_URI").unwrap();
     let connection_options = ClientOptions::parse_with_resolver_config(mongodb_uri, ResolverConfig::cloudflare()).await.expect("Failed to create connection options with cloudfare...");
-    let client = mongodb::Client::with_options(connection_options).expect("Failed to connect to db.");
-  
+    let client = Data::new(mongodb::Client::with_options(connection_options).expect("Failed to connect to db."));
+    // api-request-preventing-variable - if set to true, prevents requests to riot api 
+    let server_state_data = Data::new(Mutex::new( ServerCurrentStates { ran_out_of_requests: false, }));
+
     HttpServer::new(move || {
       //initialize cors
       let cors = Cors::permissive();
           
       App::new()
-          .wrap(cors)    
-          .app_data(web::Data::new(client.clone()))    
+          .wrap(cors)
+          .app_data(Data::clone(&server_state_data))
+          .app_data(Data::clone(&client))    
           .service(synergies)
       })
       .bind(("127.0.0.1", 8080))?
